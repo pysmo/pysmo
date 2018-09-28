9825601aab86fb5eb3f00788e3fe32c508cdc8b1
Simon M. Lloyd <simon@slloyd.net>
2018-09-28 15:04:18 +0100

    Add more documentation. Add scm_version to setuptools.

--- pysmo/__init__.py
+++ pysmo/__init__.py
@@ -1,5 +1,8 @@
 """
-The pysmo package provides simple to use and useful tools for seismologists.
+The pysmo package provides some simple to use tools for seismologists.
+
+It's core components are an interface to the commonly used `SAC file format <https://ds.iris.edu/files/sac-manual/manual/file_format.html>`_,
+functions that work with this interface, and some additional useful tools.
 """
 
 from __future__ import division, absolute_import, print_function
