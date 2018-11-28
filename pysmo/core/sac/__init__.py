8ae63f6d0176c5c927ede1aed406c3155a17dcf0
Simon M. Lloyd <simon@slloyd.net>
2018-11-28 22:30:53 +0000

    Ensure SacIO is used instead of SacFile in the documentation.

--- pysmo/core/sac/__init__.py
+++ pysmo/core/sac/__init__.py
@@ -1,8 +1,8 @@
 """
-The psymo.sac package consists of two modules, which provide:
+The psymo.core.sac package consists of two modules, which provide:
 
-    1. The python class :class:`SacFile`, to read and write SAC files.
-    2. Functions which operate on the :class:`SacFile` class.
+    1. The python class :class:`SacIO`, to read and write SAC files.
+    2. Functions which operate on the :class:`SacIO` class.
 """
 from __future__ import division, absolute_import, print_function
 name = 'sac'
