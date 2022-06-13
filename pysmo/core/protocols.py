f34bbe62165ae8adacbf98fc7fc3103e6f4ad33d
Simon Lloyd <smlloyd@users.noreply.github.com>
2022-06-13 21:27:50 +0100

    Documentation - Fundamentals (#53)
    * Documentation - Fundamentals
    
    * Remove whitespace to fix linting, add ignoring type back

--- pysmo/core/protocols.py
+++ pysmo/core/protocols.py
@@ -1,28 +1,31 @@
 """
-A core principle of pysmo (or more specifically pysmo functions), is to distinguish between how data are
+Pysmo data types serve as input and output in many Pysmo functions.
+
+
+A core principle of Pysmo (or more specifically Pysmo functions), is to distinguish between how data are
 stored on disk, and how they are processed. Most computations will only use a small subset of the data
 contained in a file. For example, a function that calculates the distance between two points (e.g. signal
 source and instrument location) only needs the coordinates of these points. Other data that may be stored
 in the file are irrelevant to the computation and can be ignored. It therefore makes sense to break up
 the typically complex data structures used in files to simpler, more meaningful ones that serve as the
-building blocks for pysmo functions. Of course, this needs to happen in a well defined way. In pysmo we
+building blocks for Pysmo functions. Of course, this needs to happen in a well defined way. In Pysmo we
 achieve this using `Python Protocol classes <https://docs.python.org/3/library/typing.html#typing.Protocol>`_.
 These protocols define the types of data that can be used in functions much like built in types such as
 integers or strings. The main considerations, benefits, and implications of this approach are:
 
-*   The different types of input data that are available to pysmo functions via the protocols are well
+*   The different types of input data that are available to Pysmo functions via the protocols are well
     defined and typically much simpler than the file format used to store the data, making it very
-    straightforward to use, maintain and extend pysmo.
-*   Files of various formats still need to be read into python classes to be used with pysmo,
+    straightforward to use, maintain and extend Pysmo.
+*   Files of various formats still need to be read into python classes to be used with Pysmo,
     but the way they are allowed to be used is determined entirely by the protocols matched
-    by these classes. In other words, compatibility of multiple file formats with pysmo functions
+    by these classes. In other words, compatibility of multiple file formats with Pysmo functions
     can be guaranteed (so much so that mixing and matching different file formats in the same
     function is seamlessly possible).
-*   No need for a "native" pysmo data class, which would likely require importing (and exporting)
+*   No need for a "native" Pysmo data class, which would likely require importing (and exporting)
     data from other formats. Instead, existing formats can be extended to become compatible with
-    pysmo protocols.
+    Pysmo protocols.
 *   Since traditional file formats contain a lot of (meta)data, the corresponding data classes in
-    pysmo will often match multiple protocol types.
+    Pysmo will often match multiple protocol types.
 
 .. note::
     Python does not perform run-time type checking (though there are libraries that can provide this).
