aaaf043c17ffcea5defa4c30c9706d49379132e6
Omkar Ranadive <52380618+Omkar-Ranadive@users.noreply.github.com>
2023-06-25 08:20:51 -0700

    Normalize function and test file for the func (#76)
    * Normalize function and test file for the func
    
    * Make normalize compatible with nested classes.
    
    - Update docstring in normalize function
    - Fix types in test_normalize.py
    
    ---------
    
    Co-authored-by: Simon Lloyd <smlloyd@gmail.com>

---
+++ pysmo/functions/normalize.py
@@ -0,0 +1,27 @@
+import copy
+from pysmo import Seismogram
+import numpy as np
+
+
+def normalize(seismogram: Seismogram) -> Seismogram:
+    """
+    Normalize the seismogram with its absolute max value
+
+    :param seismogram: Seismogram object.
+    :type seismogram: pysmo.Seismogram
+    :returns: Normalized seismogram.
+    :rtype: pysmo.Seismogram
+
+    Example usage::
+        >>> import numpy as np
+        >>> from pysmo import SAC, detrend
+        >>> original_seis = SAC.from_file('sacfile.sac').Seismogram
+        >>> normalized_seis = normalize(original_seis)
+        >>> assert np.max(normalized_seis.data) <= 1
+        True
+    """
+    seis = copy.deepcopy(seismogram)
+    norm = np.max(seis.data)
+    seis.data /= np.abs(norm)
+
+    return seis
