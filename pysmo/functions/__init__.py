75c665e8ed50d082711599e3b0bfb52c23ec467f
Omkar Ranadive <omkar.r1997@gmail.com>
2023-08-10 15:52:57 -0700

    Cross correlation function and associated test files

--- pysmo/functions/__init__.py
+++ pysmo/functions/__init__.py
@@ -9,6 +9,7 @@ from .filter_gauss import envelope, gauss
 from .plotseis import plotseis
 from .azdist import azimuth, backazimuth, distance
 from .normalize import normalize
+from .xcorr import xcorr
 
 __all__ = [
     'detrend',
@@ -19,5 +20,6 @@ __all__ = [
     'azimuth',
     'backazimuth',
     'distance',
-    'normalize'
+    'normalize',
+    'xcorr'
 ]
