3d0f26e39d28dc3f40d9e09fbbe00c915892c559
Omkar Ranadive <omkar.r1997@gmail.com>
2023-09-28 15:34:43 -0500

    Cross Correlation function

--- pysmo/tools/signal.py
+++ pysmo/tools/signal.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pysmo import Seismogram, MiniSeismogram
+from scipy.signal import correlate  # type: ignore
 
 
 def envelope(seismogram: Seismogram, Tn: float, alpha: float) -> MiniSeismogram:
@@ -64,3 +65,60 @@ def __gauss(seis: Seismogram, Tn: float, alpha: float) -> tuple[np.ndarray, np.n
     qn = np.fft.ifft(Qn).real
     an = np.sqrt(hn**2 + qn**2)  # envelope
     return (an, hn)
+
+
+def xcorr(
+    seis1: Seismogram, seis2: Seismogram, lag: float, normalize: bool = True
+) -> np.ndarray:
+    """
+    The function cross-correlates two real arrays, denoted as 'a' and 'v,' utilizing the
+    [scipy.signal.correlate function](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.correlate.html#scipy.signal.correlate)
+    with the 'valid' mode. In this mode, the resulting array's length is equal to 2 times
+    the difference between the lengths of 'a' and 'v.' This operation is often applied to
+    scenarios where 'a' represents a time series, while 'v' serves as a template. If you
+    intend to perform cross-correlation between two signals, each with a duration of 'z'
+    units along the x-axis (e.g., seconds), spanning both negative and positive shifts
+    (lags) of 'y' units on the x-axis (e.g., seconds), it is crucial to ensure that the
+    'a' array commences at x=-y and concludes at x=z+y. The 'v' array, representing the
+    template, may begin at the same starting point or, alternatively, initiate at x=0 and
+    terminate at x=z. Here, 'z' denotes the signal's duration, while 'y' signifies the
+    maximum lag of interest, both in the positive and negative directions. The output is
+    normalized with autocorrelation of v (squared norm of v).
+
+    Parameters:
+        seis1: Seismogram object containing array a.
+        seis2: Seismogram object containing array v.
+        lag: Lag duration in seconds.
+        normalize: If true, returned array is normalized.
+
+    Returns:
+        Cross correlated numpy array.
+
+    Examples:
+        >>> from pysmo import SAC
+        >>> from pysmo.tools.signal import xcorr
+        >>> seis = SAC.from_file('sacfile.sac').seismogram
+        >>> lag = 20 # Lag in seconds
+        >>> # Create a lagged version of the original signal
+        >>> shift = int(lag/seis.delta)
+        >>> seis_lagged = MiniSeismogram.clone(seis, skip_data=True)
+        >>> seis_lagged.data = np.roll(seis.data, shift)
+        >>> # Cross Correlate the original signal and the lagged version
+        >>> corr_arr = xcorr(seis, seis_lagged, lag)
+    """
+
+    a, v = seis1.data, seis2.data
+    lagsamp = round(lag / seis1.delta)
+
+    if len(a) >= len(v):
+        v2 = v[lagsamp : len(v) - lagsamp] if len(a) == len(v) else v
+        autov = np.dot(v2, v2)
+        xc = correlate(a, v2, mode="valid")
+    else:
+        autov = np.dot(v, v)
+        xc = correlate(v, a, mode="valid")[::-1]
+
+    if not normalize:
+        autov = 1
+
+    return xc / autov
--- tests/tools/test_signal.py
+++ tests/tools/test_signal.py
@@ -1,5 +1,5 @@
 from tests.conftest import TESTDATA
-from pysmo.tools.signal import gauss, envelope
+from pysmo.tools.signal import gauss, envelope, xcorr
 from pysmo import Seismogram, plotseis, SAC, MiniSeismogram
 import matplotlib.pyplot as plt  # type: ignore
 import pytest
@@ -55,3 +55,96 @@ def test_plot_gauss_env(seismogram: Seismogram = SACSEIS) -> plt.figure:
     env_seis = envelope(seismogram, Tn, alpha)
     fig = plotseis(seismogram, gauss_seis, env_seis, showfig=False)
     return fig
+
+
+@pytest_cases.parametrize(
+    "seismogram", (SACSEIS, MINISEIS), ids=("SacSeismogram", "MiniSeismogram")
+)
+def test_xcorr(seismogram: Seismogram) -> None:
+    duration = 100  # seconds
+    delta = 0.025  # x-axis step
+    xbase = np.arange(0, duration, delta)
+    noise = np.random.default_rng(seed=42).normal(0, 0.2, len(xbase))
+    a = (
+        np.exp(-0.5 * ((xbase - 12) / 1) ** 2)
+        - 0.3 * np.exp(-0.5 * ((xbase - 16) / 1) ** 2)
+        + noise
+    )
+    lag_times = [10, 12.5, 17]
+
+    for testcase in range(3):
+        if testcase == 0:
+            x = xbase
+            v = np.exp(-0.5 * ((x - 18) / 1) ** 2)
+            expected_arr = [
+                0.039453,
+                0.040382,
+                0.041337,
+                0.042319,
+                0.043331,
+                0.044373,
+                0.045447,
+                0.046555,
+                0.047698,
+                0.048879,
+            ]
+
+        if testcase == 1:
+            ind = round(len(xbase) / 8)
+            x = xbase[ind:-ind]
+            v = np.exp(-0.5 * ((x - 18) / 1) ** 2)
+            expected_arr = [
+                -0.006293,
+                -0.006286,
+                -0.006272,
+                -0.006248,
+                -0.006215,
+                -0.006173,
+                -0.006121,
+                -0.006059,
+                -0.005986,
+                -0.005902,
+            ]
+
+        if testcase == 2:
+            x = np.arange(-17, duration + 17, delta)
+            v = np.exp(-0.5 * ((x - 18) / 1) ** 2)
+            expected_arr = [
+                -0.002352,
+                -0.002758,
+                -0.003177,
+                -0.003607,
+                -0.004049,
+                -0.004502,
+                -0.004965,
+                -0.005439,
+                -0.005922,
+                -0.006414,
+            ]
+
+        seis1 = MiniSeismogram(data=a, delta=delta)
+        seis2 = MiniSeismogram(data=v, delta=delta)
+        corr_arr = xcorr(seis1, seis2, lag_times[testcase])
+
+        assert np.allclose(np.round(corr_arr[:10], decimals=6), np.array(expected_arr))
+
+    # Test on sac files
+    lag = 20
+    shift = int(lag / seismogram.delta)
+    seismogram_lagged = MiniSeismogram.clone(seismogram, skip_data=True)
+    seismogram_lagged.data = np.roll(seismogram.data, shift)
+    corr_arr = xcorr(seismogram, seismogram_lagged, lag)
+    expected_arr = [
+        0.99999994,
+        0.9998093,
+        0.9995737,
+        0.9994081,
+        0.999202,
+        0.9990382,
+        0.99889684,
+        0.99881154,
+        0.99875396,
+        0.99869645,
+    ]
+
+    assert np.allclose(np.round(corr_arr[:10], decimals=6), np.array(expected_arr))
