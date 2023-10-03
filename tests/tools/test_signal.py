0713f6f703393beb017c9f329532e909665acc0f
Simon Lloyd <smlloyd@gmail.com>
2023-10-03 16:53:45 +0100

    chore: add py312 to matrix

--- tests/tools/test_signal.py
+++ tests/tools/test_signal.py
@@ -1,7 +1,7 @@
 from tests.conftest import TESTDATA
 from pysmo.tools.signal import gauss, envelope, xcorr
 from pysmo import Seismogram, plotseis, SAC, MiniSeismogram
-import matplotlib.pyplot as plt  # type: ignore
+import matplotlib.figure
 import pytest
 import pytest_cases
 import numpy as np
@@ -47,7 +47,7 @@ def test_gauss(seismogram: Seismogram) -> None:
 
 @pytest.mark.depends(on=["test_envelope", "test_gauss"])
 @pytest.mark.mpl_image_compare(remove_text=True, baseline_dir="../baseline/")
-def test_plot_gauss_env(seismogram: Seismogram = SACSEIS) -> plt.figure:
+def test_plot_gauss_env(seismogram: Seismogram = SACSEIS) -> matplotlib.figure.Figure:
     Tn = 50  # Center Gaussian filter at 50s period
     alpha = 50  # Set alpha (which determines filterwidth) to 50
     seismogram.data = seismogram.data - np.mean(seismogram.data)
