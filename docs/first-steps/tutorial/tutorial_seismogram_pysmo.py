d207256c8207ee20f006f6e7ce48c9821c50e1e2
Simon Lloyd <smlloyd@gmail.com>
2025-04-07 17:51:20 +0100

    docs: update tutorial

---
+++ docs/first-steps/tutorial/tutorial_seismogram_pysmo.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass, field
+from tutorial_seismogram import TutorialSeismogram
+
+
+@dataclass
+class TutorialStation:
+    parent: TutorialSeismogram
+
+    @property
+    def latitude(self) -> float:
+        if self.parent.station_latitude is None:
+            raise ValueError("parent value may not be None")
+        return self.parent.station_latitude
+
+    @latitude.setter
+    def latitude(self, value: float):
+        self.parent.station_latitude = value
+
+    @property
+    def longitude(self) -> float:
+        if self.parent.station_longitude is None:
+            raise ValueError("parent value may not be None")
+        return self.parent.station_longitude
+
+    @longitude.setter
+    def longitude(self, value: float):
+        self.parent.station_longitude = value
+
+
+@dataclass
+class TutorialEvent:
+    parent: TutorialSeismogram
+
+    @property
+    def latitude(self) -> float:
+        if self.parent.event_latitude is None:
+            raise ValueError("parent value may not be None")
+        return self.parent.event_latitude
+
+    @latitude.setter
+    def latitude(self, value: float):
+        self.parent.event_latitude = value
+
+    @property
+    def longitude(self) -> float:
+        if self.parent.event_longitude is None:
+            raise ValueError("parent value may not be None")
+        return self.parent.event_longitude
+
+    @longitude.setter
+    def longitude(self, value: float):
+        self.parent.event_longitude = value
+
+
+@dataclass
+class TutorialSeismogramPysmo(TutorialSeismogram):
+    station: TutorialStation = field(init=False)
+    event: TutorialEvent = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.station = TutorialStation(parent=self)
+        self.event = TutorialEvent(parent=self)
