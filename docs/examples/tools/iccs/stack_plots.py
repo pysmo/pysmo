113c16021fc845b37c2b3d39ea4ab7e2f2b26e4f
Simon Lloyd <smlloyd@gmail.com>
2025-09-02 01:39:02 +0100

    feat: add padding to iccs plots

--- docs/examples/tools/iccs/stack_plots.py
+++ docs/examples/tools/iccs/stack_plots.py
@@ -1,50 +1,48 @@
 from pysmo.tools.iccs import ICCS, plotstack
 from make_iccs_data import example_data
-import matplotlib
 import matplotlib.pyplot as plt
 
 
-matplotlib.use("TkAgg")
-
-
 def make_plots(dark: bool = False, savefig: bool = False) -> None:
-    if dark:
+    if dark is True:
         plt.style.use("dark_background")
+
     iccs_seismograms = example_data()
     iccs = ICCS(iccs_seismograms)
-    fig = plotstack(iccs)
+    fig = plotstack(iccs, False)
     fname = "docs/examples/tools/iccs/stack_initial.png"
-    if dark:
+    if dark is True:
         fname = fname.replace(".png", "_dark.png")
     if savefig:
         fig.savefig(fname, transparent=True)
 
     iccs()
-    fig = plotstack(iccs)
+    fig = plotstack(iccs, False)
     fname = "docs/examples/tools/iccs/stack_first_run.png"
-    if dark:
+    if dark is True:
         fname = fname.replace(".png", "_dark.png")
     if savefig:
         fig.savefig(fname, transparent=True)
 
     iccs(autoselect=True)
-    fig = plotstack(iccs)
+    fig = plotstack(iccs, False)
     fname = "docs/examples/tools/iccs/stack_autoselect.png"
-    if dark:
+    if dark is True:
         fname = fname.replace(".png", "_dark.png")
     if savefig:
         fig.savefig(fname, transparent=True)
 
     iccs(autoflip=True)
-    fig = plotstack(iccs)
+    fig = plotstack(iccs, False)
     fname = "docs/examples/tools/iccs/stack_autoflip.png"
-    if dark:
+    if dark is True:
         fname = fname.replace(".png", "_dark.png")
     if savefig:
         fig.savefig(fname, transparent=True)
 
 
 if __name__ == "__main__":
+    make_plots()
     make_plots(dark=True)
+    # make_plots(savefig=True)
     # make_plots(dark=True, savefig=True)
-    # make_plots(dark=False, savefig=True)
