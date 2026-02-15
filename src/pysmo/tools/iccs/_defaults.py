b2b14248628859cdbd036b4724c8fcb6f3541da6
Simon Lloyd <smlloyd@users.noreply.github.com>
2026-02-15 17:57:47 +0000

    refactor(tools-iccs): Clarify types of prepared seismograms used in iccs

--- src/pysmo/tools/iccs/_defaults.py
+++ src/pysmo/tools/iccs/_defaults.py
@@ -1,12 +1,9 @@
+from ._types import CONVERGENCE_METHOD
 from dataclasses import dataclass, field
 from matplotlib.colors import Colormap
 from datetime import timedelta
-from typing import TYPE_CHECKING
 import matplotlib as mpl
 import numpy as np
-
-if TYPE_CHECKING:
-    from ._iccs import CONVERGENCE_METHOD
 
 
 @dataclass(frozen=True)
@@ -16,27 +13,24 @@ class _IccsDefaults:
     # ------------------------------------------------------------------------
     # ICCS attribute defaults
     # ------------------------------------------------------------------------
-
-    WINDOW_PRE: timedelta = timedelta(seconds=-15)
-    WINDOW_POST: timedelta = timedelta(seconds=15)
-    PLOT_PADDING: timedelta = timedelta(seconds=20)
-    RAMP_WIDTH: timedelta | float = 0.0
-    MIN_CCNORM: np.floating | float = 0.5
+    window_pre: timedelta = timedelta(seconds=-15)
+    window_post: timedelta = timedelta(seconds=15)
+    context_width: timedelta = timedelta(seconds=20)
+    ramp_width: timedelta | float = 0.1
+    min_ccnorm: np.floating | float = 0.5
 
     # ------------------------------------------------------------------------
     # ICCS __call__ defaults
     # ------------------------------------------------------------------------
-
-    CONVERGENCE_LIMIT: float = 10e-6
-    CONVERGENCE_METHOD: "CONVERGENCE_METHOD" = "corrcoef"
-    MAX_ITER: int = 10
+    convergence_limit: float = 1e-5
+    convergence_method: CONVERGENCE_METHOD = CONVERGENCE_METHOD.corrcoef
+    max_iter: int = 10
 
     # ------------------------------------------------------------------------
     # function defaults
     # ------------------------------------------------------------------------
-
-    IMG_CMAP: Colormap = field(default_factory=lambda: mpl.colormaps["RdBu"])
-    STACK_CMAP: Colormap = field(default_factory=lambda: mpl.colormaps["cool"])
+    img_cmap: Colormap = field(default_factory=lambda: mpl.colormaps["RdBu"])
+    stack_cmap: Colormap = field(default_factory=lambda: mpl.colormaps["cool"])
 
 
 ICCS_DEFAULTS = _IccsDefaults()
