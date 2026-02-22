9dfb27b0bf805135b447b4646ed0f27ca747e374
Simon Lloyd <smlloyd@gmail.com>
2026-02-22 00:38:14 +0000

    fix: distinguish between required/non-required sac time headers

--- src/pysmo/lib/io/_sacio/__init__.py
+++ src/pysmo/lib/io/_sacio/__init__.py
@@ -1,4 +1,13 @@
-from ._sacio_rendered import SAC_TIME_HEADERS
+from ._sacio_rendered import (
+    SAC_TIME_HEADERS,
+    SAC_REQUIRED_TIME_HEADERS,
+    SAC_OPTIONAL_TIME_HEADERS,
+)
 from .sacio import SacIO
 
-__all__ = ["SacIO", "SAC_TIME_HEADERS"]
+__all__ = [
+    "SacIO",
+    "SAC_TIME_HEADERS",
+    "SAC_REQUIRED_TIME_HEADERS",
+    "SAC_OPTIONAL_TIME_HEADERS",
+]
